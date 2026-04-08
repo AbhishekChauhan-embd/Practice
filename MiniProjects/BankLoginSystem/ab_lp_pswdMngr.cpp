f56333fd91c9bb75447efa3c324d3cb11110f0fd
AbhishekChauhan-embd <abhishek.embd@gmail.com>
2026-04-08 22:47:31 +0530

    Project branch created

--- MiniProjects/BankLoginSystem/ab_lp_pswdMngr.cpp
+++ MiniProjects/BankLoginSystem/ab_lp_pswdMngr.cpp
@@ -5,4 +5,6 @@ void printFromPswdMngr()
 {
         cout << "----------Welcome to Password Manager----------\n";
 
+        cout << "This is a simple password manager that allows you to securely store and manage your passwords for various accounts. You can add new passwords, view existing passwords, and delete passwords as needed. The password manager uses encryption to protect your data and ensure that your passwords are safe from unauthorized access.\n";
+
 }
